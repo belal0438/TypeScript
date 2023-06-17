4c773b0ad73597dca0ddc7d543eb1131e03876d4
Belal <mohammadvns121@gmail.com>
2023-06-17 12:12:10 +0530

    Type Inference & Type Casting

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-// function add(num1, num2){
-//     return num1+num2
-// }
-// console.log(add(1,2));
-// console.log(add("1","2"));
+var num1Element = document.getElementById('num1');
+var num2Element = document.getElementById('num2');
+var buttonElement = document.querySelector('button');
 function add(num1, num2) {
     return num1 + num2;
 }
-console.log(add(1, 2));
+buttonElement.addEventListener('click', function () {
+    var num1 = num1Element.value;
+    var num2 = num2Element.value;
+    var result = add(+num1, +num2);
+    console.log(result);
+});
