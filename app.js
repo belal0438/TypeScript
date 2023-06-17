56d87b3adb7e2189f292d85eb11d02e052625449
Belal <mohammadvns121@gmail.com>
2023-06-17 17:22:28 +0530

    TypeScript with node js

--- app.js
+++ app.js
@@ -1,36 +1,12 @@
 "use strict";
-const Num1Element = document.getElementById('num1');
-const Num2Element = document.getElementById('num2');
-const ButtonElement = document.querySelector('button');
-const numResults = [];
-const textResults = [];
-function Add(num1, num2) {
-    if (typeof num1 === 'number' && typeof num2 === 'number') {
-        return num1 + num2;
-    }
-    else if (typeof num1 === 'string' && typeof num2 === 'string') {
-        return num1 + " " + num2;
-    }
-    return +num1 + +num2;
-}
-function printResult(resultObj) {
-    console.log(resultObj.val);
-}
-ButtonElement.addEventListener('click', () => {
-    const num1 = Num1Element.value;
-    const num2 = Num2Element.value;
-    const result = Add(+num1, +num2);
-    numResults.push(result);
-    const Stringresult = Add(num1, num2);
-    textResults.push(Stringresult);
-    printResult({ val: result, timestamp: new Date() });
-    console.log(numResults, textResults);
-});
-const myPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('It worked!');
-    }, 100);
-});
-myPromise.then((result) => {
-    console.log(result.split('w'));
-});
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const todos_1 = __importDefault(require("./routes/todos"));
+const app = (0, express_1.default)();
+app.use(body_parser_1.default.json());
+app.use(todos_1.default);
+app.listen(3000);
--- app.ts
+++ app.ts
@@ -1,62 +1,11 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import todosRoutes from './routes/todos';
 
-const Num1Element = document.getElementById('num1') as HTMLInputElement;
-const Num2Element = document.getElementById('num2') as HTMLInputElement;
-const ButtonElement = document.querySelector('button')!;
-
-const numResults: Array<number> = [];
-const textResults: string[] = [];
+const app = express();
 
 
+app.use(bodyParser.json());
+app.use(todosRoutes);
 
-type NumOrString = number | string;
-
-
-function Add(num1: NumOrString, num2: NumOrString) {
-    if (typeof num1 === 'number' && typeof num2 === 'number') {
-        return num1 + num2;
-    } else if (typeof num1 === 'string' && typeof num2 === 'string') {
-        return num1 + " " + num2;
-    }
-    return +num1 + +num2;
-}
-
-
-type Result = { val: number; timestamp: Date }
-
-interface ResultObj {
-    val: number;
-    timestamp: Date;
-}
-
-function printResult(resultObj: ResultObj) {
-    console.log(resultObj.val)
-}
-
-
-
-
-ButtonElement.addEventListener('click', () => {
-    const num1 = Num1Element.value;
-    const num2 = Num2Element.value;
-    const result = Add(+num1, +num2)
-    numResults.push(result as number)
-
-    const Stringresult = Add(num1, num2)
-    textResults.push(Stringresult as string)
-
-
-    printResult({ val: result as number, timestamp: new Date() })
-
-    console.log(numResults, textResults);
-});
-
-
-const myPromise = new Promise<string>((resolve, reject) => {
-    setTimeout(() => {
-        resolve('It worked!');
-    }, 100)
-});
-
-myPromise.then((result) => {
-    console.log(result.split('w'));
-})
+app.listen(3000);
